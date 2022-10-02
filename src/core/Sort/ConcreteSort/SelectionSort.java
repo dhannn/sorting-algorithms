bc8cb94a3eb2ca1bad945321981ffcf18ab76fe7
Daniel Ramos <daniel_ramos@dlsu.edu.ph>
2022-10-02 11:59:29 +0800

    add documentions in sorts

--- src/core/Sort/ConcreteSort/SelectionSort.java
+++ src/core/Sort/ConcreteSort/SelectionSort.java
@@ -3,22 +3,26 @@ package core.sort.ConcreteSort;
 import core.sequence.Sequence;
 import core.sort.Sorter;
 
-
+/**
+ * Concrete implementation of Sorter. It sorts suffixes based on a modified 
+ * version of selection sort.
+ */
 public class SelectionSort extends Sorter
 {
+    /**
+     * {@inheritDoc}
+     * 
+     */
     @Override
     public void sort(Sequence baseSequence, int[] suffixes)
     {
-        for(int i = 0; i < baseSequence.getLength(); i++)
+        for(int i = 0; i < suffixes.length - 1; i++)
         {
             int min = i;
 
             for(int j = i + 1; j < suffixes.length; j++)
             {
-                String a = baseSequence.getSuffix(j);
-                String b = baseSequence.getSuffix(min);
-                
-                if(compare(a, b) < 0)
+                if(baseSequence.compareTo(suffixes[j], suffixes[min]) < 0)
                     min = j;
             }
 
--- src/core/Sort/Sorter.java
+++ src/core/Sort/Sorter.java
@@ -2,31 +2,31 @@ package core.sort;
 
 import core.sequence.Sequence;
 
+/**
+ * An abstract class that sorts the suffixes of a given base sequence.
+ */
 public abstract class Sorter
 {
+    /**
+     * Uses the baseSequence to sort the suffixes and store the result in an array
+     * 
+     * @param baseSequence
+     * @param suffixes
+     */
     public abstract void sort(Sequence baseSequence, int[] suffixes);
     
+    /**
+     * Swaps two elements in array of suffixes
+     * 
+     * @param suffixes  integer representation of the suffix (i.e starting index)
+     * @param a         index of first element to be swapped
+     * @param b         index of second element to be swapped
+     */
     public static void swap(int[] suffixes, int a, int b)
     {
         int temp = suffixes[a];
         
         suffixes[a] = suffixes[b];
         suffixes[b] = temp;
-    }
-
-    public static int compare(Sequence sequence, int[] suffixes, int a, int b)
-    {
-        int indexA = suffixes[a];
-        int indexB = suffixes[b];
-
-        String strA = sequence.getSuffix(indexA);
-        String strB = sequence.getSuffix(indexB);
-
-        return strA.compareToIgnoreCase(strB);
-    }
-
-    public static int compare(String a, String b)
-    {
-        return a.compareToIgnoreCase(b);
     }
 }
