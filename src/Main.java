4c582e5028089a3b66541e546a97ba7247a17776
Daniel Ramos <daniel_ramos@dlsu.edu.ph>
2022-10-07 22:22:58 +0800

    Fix quicksort bug

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ import java.io.IOException;
 
 import core.data.Experiment;
 import core.sort.ConcreteSort.MergeSort;
+import core.sort.ConcreteSort.QuickSort;
 import core.sort.ConcreteSort.SelectionSort;
 // import log.RawDataLogger;
 // import log.CSVLogger.AverageLogger;
@@ -13,7 +14,7 @@ public class Main
 {
     public static void main(String[] args) throws IOException
     {
-        Experiment testSelectionSort = new Experiment(new MergeSort(), "actg", 100);
+        Experiment testSelectionSort = new Experiment(new QuickSort(), "actg", 100);
         testSelectionSort.execute();
         RuntimeLogger sumLogger = new RuntimeLogger(testSelectionSort);
         AverageLogger aveLogger = new AverageLogger(testSelectionSort);
